d9854b37f85634781bc13568f7c84e3e053bfee8
Marc <marcnebot99@gmail.com>
2019-01-28 16:58:54 +0100

    Add order dialog added

--- app/src/main/java/org/ieselcaminas/pro/myfirstproject/FragmentOrders.java
+++ app/src/main/java/org/ieselcaminas/pro/myfirstproject/FragmentOrders.java
@@ -1,13 +1,24 @@
 package org.ieselcaminas.pro.myfirstproject;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FragmentOrders extends Fragment {
+
+    FloatingActionButton fab;
+    Button buttonDiaSubmit;
+    Button buttonDiaCancel;
+    EditText editTextTitle;
+    EditText editTextDescription;
 
     public FragmentOrders() {
 
@@ -18,7 +29,48 @@ public class FragmentOrders extends Fragment {
         super.onCreate(savedInstanceState);
         final View thisView = inflater.inflate(R.layout.fragment_orders, container, false);
 
+        fab = thisView.findViewById(R.id.fab);
 
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                final AlertDialog dialogBuilder = new AlertDialog.Builder(thisView.getContext()).create();
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_add_order, null);
+
+                editTextTitle = dialogView.findViewById(R.id.editTextTitle);
+                editTextDescription = dialogView.findViewById(R.id.editTextDescr);
+                buttonDiaSubmit = dialogView.findViewById(R.id.buttonSubmit);
+                buttonDiaCancel = dialogView.findViewById(R.id.buttonCancel);
+
+                buttonDiaSubmit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (editTextTitle.length() == 0 || editTextDescription.length() == 0) {
+                            Toast.makeText(thisView.getContext(), "Please put some information", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            dialogBuilder.dismiss();
+                        }
+
+                    }
+                });
+                buttonDiaCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialogBuilder.dismiss();
+                    }
+                });
+
+                dialogBuilder.setView(dialogView);
+                dialogBuilder.show();
+
+
+            }
+        });
         return thisView;
     }
 }
